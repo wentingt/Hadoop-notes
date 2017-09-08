a99b67af6dde4f389c5521bcd435917b23202297
Wenting Tan <wentingt@umich.edu>
2017-09-08 16:33:02 -0400

    am

--- addGit.sh
+++ addGit.sh
@@ -2,6 +2,7 @@
 
 git init
 git add --all
-git remote set-url origin https://github.com/wentingt/Hadoop-notes.git 
+#git remote set-url origin https://github.com/wentingt/Hadoop-notes.git 
+git remote add origin https://github.com/wentingt/Hadoop-notes.git 
 git commit -am "am"
 git push -u origin master
